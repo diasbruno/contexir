829c0001be405e3f8b7b8bb7574c6651ef495fd2
Bruno Dias <dias.h.bruno@gmail.com>
2025-12-08 16:27:37 -0300

    updated mix package fields.

--- mix.exs
+++ mix.exs
@@ -3,12 +3,21 @@ defmodule Contexir.MixProject do
 
   def project do
     [
-      app: :contexir,
+      name: "Contexir",
+      source_url: "https://github.com/diasbruno/contexir",
       version: "0.1.0",
       elixir: "~> 1.18",
+      app: :contexir,
+      description: description(),
+      build_embedded: Mix.env == :prod,
       start_permanent: Mix.env() == :prod,
+      package: package(),
       deps: deps()
     ]
+  end
+
+  defp description() do
+    "Context-oriented programming in Elixir"
   end
 
   def application do
