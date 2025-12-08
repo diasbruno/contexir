41ec7c1b963671506b00a0caf7faa7488bb27c04
Bruno Dias <dias.h.bruno@gmail.com>
2025-12-08 16:31:55 -0300

    fixed package files.

--- mix.exs
+++ mix.exs
@@ -23,8 +23,8 @@ defmodule Contexir.MixProject do
   defp package() do
     [
       name: "contexir",
-      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
-        license* CHANGELOG* changelog* src),
+      files: ~w(lib .formatter.exs mix.exs README* readme* LICENSE*
+        license*),
       licenses: ["Unlicense"],
       links: %{"GitHub" => "https://github.com/diasbruno/contexir"}
     ]
