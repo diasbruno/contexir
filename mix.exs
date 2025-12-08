2133a1e7c3648a8668cc0e3e694e7a60490a82db
Bruno Dias <dias.h.bruno@gmail.com>
2025-12-08 20:23:26 -0300

    updated version 0.1.7.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Contexir.MixProject do
     [
       name: "Contexir",
       source_url: "https://github.com/diasbruno/contexir",
-      version: "0.1.6",
+      version: "0.1.7",
       elixir: "~> 1.18",
       app: :contexir,
       description: description(),
