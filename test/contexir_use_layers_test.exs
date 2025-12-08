1294bba637d50c42e654fe842d2a715e9362495c
Bruno Dias <dias.h.bruno@gmail.com>
2025-12-08 15:03:59 -0300

    added tsts for use_layers.

---
+++ test/contexir_use_layers_test.exs
@@ -0,0 +1,59 @@
+defmodule ContexirUseLayersTest do
+  use ExUnit.Case, async: false
+  import ExUnit.CaptureIO
+  import Contexir.Macros
+  require Contexir
+
+  defmodule Target do
+    use Contexir.Base
+
+    def execute(acc, ctx) do
+      IO.inspect("Primary")
+      %{acc | string: acc.string <> " target_part"}
+    end
+  end
+
+  deflayer BeforeAfter do
+    defpartial ContexirUseLayersTest.Target.execute(acc, ctx), mode: :around do
+      IO.inspect("a around")
+      %{string: s} = acc
+      result = continue(
+        ContexirUseLayersTest.Target,
+        :execute,
+        [%{string: s <> " around before-after_part"}])
+      IO.inspect("a around end")
+      result
+    end
+
+    defpartial ContexirUseLayersTest.Target.execute(acc, ctx), mode: :before do
+      IO.inspect("a before")
+      Process.put(:target, 1)
+    end
+
+    defpartial ContexirUseLayersTest.Target.execute(acc, ctx), mode: :after do
+      IO.inspect("a after")
+      x = Process.get(:target) || 0
+      Process.put(:target, x - 1)
+    end
+  end
+
+  deflayer UseLayer do
+    use_layers [ContexirUseLayersTest.BeforeAfter]
+  end
+
+  #
+  # Tests
+  #
+
+  # @tag :skip
+  test "run before/after layer" do
+    layers = [ContexirUseLayersTest.UseLayer]
+
+    %{string: b} =
+      Contexir.with_layers layers,
+      ContexirUseLayersTest.Target.execute(%{string: "start_string"}, %{user: "Alice"})
+    x = Process.get(:target)
+    assert x == 0
+    assert b == "start_string around before-after_part target_part"
+  end
+end
